1aca4126a3ea8de73dc10977e51a98db8e88252c
meislifka <meislifka@gmail.com>
2024-05-19 23:24:24 -0400

    Added login function

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -25,13 +25,32 @@ router.post("/register", async (req, res) => {
     const newUser = new UserModel({ username, password: hashedPassword });
     await newUser.save();
 
-    res.json({ message: "User Registered Successfully"});
+    res.json({ message: "User Registered Successfully" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
 
-router.post("/login");
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await UserModel.findOne({ username: username });
+
+  if (!user) {
+    return res.json({ message: "User Doesn't Exist!" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.json({ message: "Username or Password is Incorrect!" });
+  }
+
+  const TOKEN = process.env.TOKEN;
+
+  const token = jwt.sign({ id: user._id }, TOKEN);
+  res.json({ token, userID: user._id });
+
+});
 
 export { router as userRouter }; 
