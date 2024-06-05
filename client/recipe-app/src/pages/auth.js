d962a2401887306a0dca466dfe4f8e7fadbf2584
meislifka <meislifka@gmail.com>
2024-06-04 22:42:58 -0400

    Added second password check

--- client/recipe-app/src/pages/auth.js
+++ client/recipe-app/src/pages/auth.js
@@ -6,21 +6,18 @@ import '../tailwind.css'; // Import Tailwind CSS
 import '../App.css';
 
 export const Auth = () => {
-  return (<div className="auth">
-    <Login />
-    <Register />
-  </div>
+  return (
+    <div className="auth">
+      <Login />
+      <Register />
+    </div>
   );
 };
 
 const Login = () => {
-  // only need access to cookie (setCookie)   access_token = name of cookie
-  const [_, setCookies] = useCookies(["access_token"])
+  const [_, setCookies] = useCookies(["access_token"]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-
-
-
   const navigate = useNavigate();
 
   const onSubmit = async (event) => {
@@ -31,18 +28,14 @@ const Login = () => {
         password,
       });
 
-      //response json being sent from route is (users.js) token, userId
       setCookies("access_token", response.data.token);
       window.localStorage.setItem("userID", response.data.userID);
 
-      //when logged in redirect to home page
       navigate("/");
     } catch (err) {
       console.error(err);
     }
-
-  }
-
+  };
 
   return (
     <Form
@@ -54,50 +47,48 @@ const Login = () => {
       onSubmit={onSubmit}
     />
   );
-
 };
-
 
 const Register = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordCheck, checkPassword] = useState("");
+  const [password2, setPassword2] = useState("");
 
   const onSubmit = async (event) => {
-    event.preventDefault(); //wont refresh page when sent
+    event.preventDefault();
+    if (password !== password2) {
+      alert("Passwords do not match!");
+      return;
+    }
 
-    //use axios to send post request to api
     try {
       await axios.post("http://localhost:3001/auth/register", {
         username,
         password,
       });
       alert("Registration Completed: Now login");
-
     } catch (err) {
       console.error(err);
-    };
+    }
+  };
 
-  }
   return (
     <Form
       username={username}
       setUsername={setUsername}
       password={password}
-      checkPassword={checkPassword}
       setPassword={setPassword}
+      password2={password2}
+      setPassword2={setPassword2}
       label="Register"
       onSubmit={onSubmit}
     />
-
   );
-
-
 };
 
-const Form = ({ username, setUsername, password, setPassword, label, onSubmit }) => {
+const Form = ({ username, setUsername, password, setPassword, password2, setPassword2, label, onSubmit, }) => {
   return (
-    < div className="flex flex-col justify-center items-center p-5 bg-white rounded shadow-md m-5 w-[400px]" >
+    <div className="flex flex-col justify-center items-center p-5 bg-[#D2B48C] rounded-xl shadow-md m-5 w-[400px] ">
       <form onSubmit={onSubmit}>
         <h2 className="font-bold text-xl">{label}</h2>
         <div className="form-group">
@@ -105,28 +96,51 @@ const Form = ({ username, setUsername, password, setPassword, label, onSubmit })
           <input
             type="text"
             id="username"
-            className="border-2 border-black"
+            className="border-2 border-black px-1"
             value={username}
             onChange={(event) => setUsername(event.target.value)}
           />
         </div>
 
         <div className="form-group">
-          <label htmlFor="password">Pasword: </label>
+          <label htmlFor="password">Password: </label>
           <input
             type="password"
             id="password"
-            className="border-2 border-black"
+            className="border-2 border-black px-1"
             value={password}
-            onChange={(event) => setPassword(event.target.value)} />
-
+            onChange={(event) => setPassword(event.target.value)}
+          />
         </div>
 
-        <button className="bg-grey border-2 border-black rounded-md px-1 hover:bg-green-500" type="submit">{label}</button>
-        {/*  <button className="text-[#46a2dc] ml-4 underline"  >  Sign up </button> */}
+        {label === "Register" && (
+          <div className="form-group">
+            <label htmlFor="password2">Confirm Password: </label>
+            <input
+              type="password"
+              id="password2"
+              className="border-2 border-black px-1"
+              value={password2}
+              onChange={(event) => setPassword2(event.target.value)}
+            />
+          </div>
+        )}
+
+        <div>
+          <button
+            className="bg-grey border-2 border-black rounded-md px-1 hover:bg-green-600"
+            type="submit"
+          >
+            {label}
+          </button>
+          {label === "Login" && (
+
+            <button className="pl-2 text-[#047a9e] font-semibold underline">Sign up</button>
+
+          )}
+        </div>
 
       </form>
-    </div >
+    </div>
   );
-
-}
+};
